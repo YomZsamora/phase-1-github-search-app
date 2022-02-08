6f8fdece7805a41435164efd49a3ae257f1eeeb5
yomZsamora <samora.y@adzumi.co.ke>
2022-02-08 23:03:03 +0300

    Fetches the GitHub user details / info using the url provided.

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const gitForm = document.querySelector("#github-form");
+
+document.addEventListener("DOMContentLoaded", () => {
+   gitForm.addEventListener("submit", searchGitHubUser);
+});
+
+// Fetches the GitHub user details / info using the url provided.
+let searchGitHubUser = (e) => {
+   e.preventDefault();
+   const username = document.querySelector('#search').value;
+   fetch(`https://api.github.com/search/users?q=${username}`)
+   .then(resp => resp.json())
+   .then(users => { renderUserDetails(users.items) })
+   .catch(error => { alert("Kuna Bug Mahali!") });
+   
+}
+
+let renderUserDetails = users => {
+   users.forEach(user => { createGitUserCard(user) })
+}
+
+let createGitUserCard = user => {
+   const gitUserCard = document.createElement("div");
+   gitUserCard.className = "user-card";
+   document.querySelector("#user-list").appendChild(gitUserCard);
+   gitUserCard.innerHTML = `<img src="${user.avatar_url}" >`
+                           + `<h2>${user.login}</h2>`
+                           + `<a class="fs11" href="${user.html_url}" target="_blank">Go to Github Profile</a><br>`
+                           + `<button class="submit-btn fs11">View ${user.login}'s Repos</button>`
+   
+}
